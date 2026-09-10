551a0d281c4e9c11a75939c6bb9ddfa74c66bddd
Pratsindia74 <pratikbagchi74@gmail.com>
2026-09-10 01:50:04 -0400

    My Shapes Sketch

---
+++ Test/SS1_MyShapes/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 400); 
+    background(220);
+    circle(100, 100, 80);
+    rect(220, 60, 120, 70);
+    triangle(60, 300, 140, 300, 100, 220);
+    line(100, 100, 100, 70);
+    line(100, 100, 130, 100);
+}
