c1450c80c8bf677a5143010046cbe5148e1b3713
Pratsindia74 <pratikbagchi74@gmail.com>
2026-09-10 00:22:02 -0400

    Test Sketch

--- README.md
+++
@@ -1,3 +0,0 @@
-# Pratik_MTECH1201-Fall26_test
-My class program mtec2026
-Fall 2026
---
+++ Test/sketch.js
@@ -0,0 +1,20 @@
+/*Pratik_MTECH1201-Fall26_test
+My class program mtec2026
+Fall 2026*/
+
+function setup()
+{
+    //set size of canvas area with width and height
+    createCanvas (800, 400);
+
+    background (127, 220, 10, 300)
+
+    line (2, 1, 3, 4);
+
+    line(500, 50, 10, 300);
+    line (0, 0, 400, 40)
+
+    traiangle (500,300, )
+}
+    
+
