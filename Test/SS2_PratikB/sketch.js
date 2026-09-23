8084f781d10758d273ee4126bdae9064fc717566
Pratsindia74 <pratikbagchi74@gmail.com>
2026-09-23 13:33:25 -0400

    Complete SS2 responsive drawing

--- Test/SS2_PratikB/sketch.js
+++ Test/SS2_PratikB/sketch.js
@@ -1,3 +1,13 @@
+// Pratik B.
+// Title: Interactive Pac-Man
+// Concept: I am exploring an interactive character that responds to the user's movement.
+// Instructions: Move the mouse to move Pac-Man and click the mouse to make Pac-Man bigger.
+
+
+let pacSize = 100;
+let eyeSize = 10;
+let moveAmount = 0;
+
 function setup() {
     createCanvas(800, 400);
 }
@@ -5,8 +15,13 @@ function setup() {
 function draw() {
     background(0);
     fill("yellow");
-    arc(mouseX, mouseY, 100, 100, 0.5, 5.8);
+    arc(mouseX, mouseY, pacSize, pacSize, 0.5, 5.8);
+    moveAmount = moveAmount + 1;
 
     fill("black");
-    circle(mouseX + 10, mouseY - 20, 10);
+    circle(mouseX + 10, mouseY - 20, eyeSize);
+}
+
+function mousePressed() {
+    pacSize = pacSize + 10;
 }
